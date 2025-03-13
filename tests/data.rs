1dc509e4e55a85c4a175231b0e5a9cbef52e772d
rami3l <rami3l@outlook.com>
2025-03-13 13:45:52 +0800

    Introduce `SpiroCpTy`

--- tests/data.rs
+++ tests/data.rs
@@ -1,22 +1,39 @@
 #[allow(unused)]
 macro_rules! test_data {
-    () => {
+    () => {{
+        use spiro::SpiroCpTy::*;
         vec![
-            SpiroCP { x: 334., y: 117., ty: 'v' },
-            SpiroCP { x: 305., y: 176., ty: 'v' },
-            SpiroCP { x: 212., y: 142., ty: 'c' },
-            SpiroCP { x: 159., y: 171., ty: 'c' },
-            SpiroCP { x: 224., y: 237., ty: 'c' },
-            SpiroCP { x: 347., y: 335., ty: 'c' },
-            SpiroCP { x: 202., y: 467., ty: 'c' },
-            SpiroCP { x: 81., y: 429., ty: 'v' },
-            SpiroCP { x: 114., y: 368., ty: 'v' },
-            SpiroCP { x: 201., y: 402., ty: 'c' },
-            SpiroCP { x: 276., y: 369., ty: 'c' },
-            SpiroCP { x: 218., y: 308., ty: 'c' },
-            SpiroCP { x: 91., y: 211., ty: 'c' },
-            SpiroCP { x: 124., y: 111., ty: 'c' },
-            SpiroCP { x: 229., y: 82., ty: 'c' },
+            SpiroCP {
+                x: 334.,
+                y: 117.,
+                ty: Corner,
+            },
+            SpiroCP {
+                x: 305.,
+                y: 176.,
+                ty: Corner,
+            },
+            SpiroCP { x: 212., y: 142., ty: G2 },
+            SpiroCP { x: 159., y: 171., ty: G2 },
+            SpiroCP { x: 224., y: 237., ty: G2 },
+            SpiroCP { x: 347., y: 335., ty: G2 },
+            SpiroCP { x: 202., y: 467., ty: G2 },
+            SpiroCP {
+                x: 81.,
+                y: 429.,
+                ty: Corner,
+            },
+            SpiroCP {
+                x: 114.,
+                y: 368.,
+                ty: Corner,
+            },
+            SpiroCP { x: 201., y: 402., ty: G2 },
+            SpiroCP { x: 276., y: 369., ty: G2 },
+            SpiroCP { x: 218., y: 308., ty: G2 },
+            SpiroCP { x: 91., y: 211., ty: G2 },
+            SpiroCP { x: 124., y: 111., ty: G2 },
+            SpiroCP { x: 229., y: 82., ty: G2 },
         ]
-    };
+    }};
 }
