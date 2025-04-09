6fc400d5c08c73935d692692033de3b7b17072e1
rami3l <rami3l@outlook.com>
2025-04-09 22:35:53 +0800

    Show range bugs with `add_mat_line()`

--- tests/run_spiro.rs
+++ tests/run_spiro.rs
@@ -1,11 +1,36 @@
 #[macro_use]
 mod data;
 
-use spiro::*;
+use spiro::{SpiroCpTy::*, *};
 
 #[test]
 fn test() {
     let mut path = test_data!();
     let oplist = run_spiro(&mut path);
-    eprintln!("{:?}", oplist);
+    eprintln!("{oplist:?}");
+}
+
+#[test]
+fn add_mat_line_no_index_out_of_bounds() {
+    let mut path = test_data!();
+    path.insert(path.len() - 2, SpiroCP { ty: End, ..path[0] });
+    let oplist = run_spiro(&mut path);
+    eprintln!("{oplist:?}");
+}
+
+#[test]
+fn add_mat_line_no_underflow() {
+    let mut path = test_data!();
+    path.drain(..2);
+    let oplist = run_spiro(&mut path);
+    eprintln!("{oplist:?}");
+}
+
+#[test]
+fn add_mat_line_no_underflow_abrupt_end() {
+    let mut path = test_data!();
+    path.reverse();
+    path.insert(1, SpiroCP { ty: End, ..path[0] });
+    let oplist = run_spiro(&mut path);
+    eprintln!("{oplist:?}");
 }
